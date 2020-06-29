215c33bc04c7acfe663c34481063675fa8611e8b
root <root@localhost.localdomain>
2020-06-29 20:34:48 +0530

    adding routings and static file

--- geodjango_test/settings.py
+++ geodjango_test/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
+
 LEAFLET_CONFIG = {
     'DEFAULT_CENTER': (20.5937, 78.9629), # SET DEFAULT PIN LOCATION
     'DEFAULT_ZOOM':5,
