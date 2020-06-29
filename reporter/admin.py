673c788753eb13b84a0ed0d227da87819ea23610
root <root@localhost.localdomain>
2020-06-29 14:09:03 +0530

    adding leaflet

--- reporter/admin.py
+++ reporter/admin.py
@@ -1,7 +1,11 @@
+from leaflet.admin import LeafletGeoAdmin
 from django.contrib import admin
 from .models import Incidences
 
-class IncidencesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'location') # user for show column name
+
+
+class IncidencesAdmin(LeafletGeoAdmin):
+    pass
+    # list_display = ('name', 'location') # user for show column name
 
 admin.site.register(Incidences, IncidencesAdmin)
